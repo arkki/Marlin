3c74a621abad7f60c14ca76ac17bfb4315eba637
arkki <arkki@arkki.tech>
2016-04-08 16:57:25 +1000

    Fixed preheat settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -728,12 +728,12 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = true; // set to true to invert the lo
 // @section temperature
 
 // Preheat Constants
-#define PLA_PREHEAT_HOTEND_TEMP 20 // CUSTOM
+#define PLA_PREHEAT_HOTEND_TEMP 200 // CUSTOM
 #define PLA_PREHEAT_HPB_TEMP 60 // CUSTOM
 #define PLA_PREHEAT_FAN_SPEED 0   // Insert Value between 0 and 255
 
 #define ABS_PREHEAT_HOTEND_TEMP 240
-#define ABS_PREHEAT_HPB_TEMP 110
+#define ABS_PREHEAT_HPB_TEMP 100
 #define ABS_PREHEAT_FAN_SPEED 0   // Insert Value between 0 and 255
 
 //==============================LCD and SD support=============================
